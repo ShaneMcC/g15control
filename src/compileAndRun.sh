855f112838e316444d8070cb97ddc5b72fa09410
ShaneMcC <ShaneMcC@d2a46293-922d-0410-af8f-97e15227588f>
2007-04-08 17:31:43 +0000

    can now fork (nohup abuse)

--- src/compileAndRun.sh
+++ src/compileAndRun.sh
@@ -1,2 +1,10 @@
 #!/bin/sh
-javac uk/org/dataforce/g15/plugins/*/*.java uk/org/dataforce/g15/G15Control.java && java uk.org.dataforce.g15.G15Control
+COMPILECOMMAND="javac uk/org/dataforce/g15/plugins/*/*.java uk/org/dataforce/g15/G15Control.java"
+RUNCOMMAND="java uk.org.dataforce.g15.G15Control"
+
+if [ "$1" = "fork" ]; then
+	$COMPILECOMMAND && nohup $RUNCOMMAND >/dev/null 2>&1 &
+	echo "Forked"	
+else 
+	$COMPILECOMMAND && $RUNCOMMAND
+fi
