2883fc7206bb54bc878aed96533724f06d121608
ShaneMcC <ShaneMcC@d2a46293-922d-0410-af8f-97e15227588f>
2007-04-08 21:47:49 +0000

    Build script now removes all class files before building Now turns all M buttons off on startup ebfore setting the default button to prevet 2 buttons being active at once

--- src/compileAndRun.sh
+++ src/compileAndRun.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
+CLEANCOMMAND="rm -Rf uk/org/dataforce/g15/*.class ; rm -Rf uk/org/dataforce/g15/*/*.class ; rm -Rf uk/org/dataforce/g15/*/*/*.class"
 COMPILECOMMAND="javac uk/org/dataforce/g15/plugins/*/*.java uk/org/dataforce/g15/G15Control.java"
 RUNCOMMAND="java uk.org.dataforce.g15.G15Control"
 
+$CLEANCOMMAND
 if [ "$1" = "fork" ]; then
 	$COMPILECOMMAND && nohup $RUNCOMMAND >/dev/null 2>&1 &
 	echo "Forked"	
